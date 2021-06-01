21154222ec235556a7ede6e82afedea2cf606923
Tetr4 <Tetr4k@gmail.com>
2021-06-01 22:14:08 +0100

    move data posting to separate thread

--- sensorread.py
+++ sensorread.py
@@ -12,6 +12,15 @@ ser = serial.Serial('/dev/ttyUSB0')
 ser2 = serial.Serial('/dev/ttyUSB1')
 ser.flushInput()
 
+def post_data(data):
+    try:
+        result = requests.post('https://emberbox.net/curing/api/readings/', json=data, timeout=1)
+        status = result.status_code
+        if status != 200: print(f'Post error code {status}')
+    except Exception as e:
+        print("Posting data failed.")
+        print(e)
+
 while True:
     try:
         ser_bytes = ser.readline()
@@ -23,34 +32,20 @@ while True:
             print('Bad data:', decoded_bytes)
             continue
 
-        msg = {"time":time.time(), "data":data}
-        print(msg['data'])
+        data['time'] = time.time()
+        print(data)
 
-        try:
-            mqttpublish.single(topic=topic, payload=json.dumps(msg), hostname=broker)
-        except:
-            print("Publishing to mqtt failed.")
-
-        try:
-            data2 = {i: msg['data'][i] for i in msg['data']}
-            data2['time'] = msg['time']
-            result = requests.post('https://emberbox.net/curing/api/readings/', json=data2)
-            status = result.status_code
-            if status != 200: print(f'Post error code {status}')
-        except KeyboardInterrupt: raise
-        except Exception as e:
-            print("Posting data failed.")
-            print(e)
-
-        if msg['data']['temperature'] > 13:
+        if data['temperature'] > 13:
             ser2.write(0b10000000.to_bytes(1, 'big'))
-        elif msg['data']['temperature'] < 12:
+        elif data['temperature'] < 12:
             ser2.write(0b00000000.to_bytes(1, 'big'))
 
-        if msg['data']['humidity'] < 80:
+        if data['humidity'] < 80:
             ser2.write(0b10000001.to_bytes(1, 'big'))
-        elif msg['data']['humidity'] > 85:
+        elif data['humidity'] > 85:
             ser2.write(0b00000001.to_bytes(1, 'big'))
+
+        threading.Thread(target=post_data, args=(data,)).start()
 
     except Exception as e:
         print(e)
